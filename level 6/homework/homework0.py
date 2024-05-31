aed877ebd1fbf00f5884cb3aa1613ed2871cf7c9
NikolozBeroshvili <nikolozbero0@gmail.com>
2024-05-31 21:27:36 +0400

    first commit

---
+++ level 6/homework/homework0.py
@@ -0,0 +1,7 @@
+
+name = "nika"
+surname = "beroshvili"
+age = 11
+text = "I am learning programming"
+
+print("My name is" + " " + name + " " + "my surname is" + " " + surname + " " + "my age is" + " " + str(age) + " " + text)
