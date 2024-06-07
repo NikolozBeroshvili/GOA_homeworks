fec7e5f8549ccbc2b7f7df6fb80a1127d3d4a13c
NikolozBeroshvili <nikolozbero0@gmail.com>
2024-06-07 12:38:17 +0400

    second commit

---
+++ level 8/classwork.py/cw0.py
@@ -0,0 +1,7 @@
+#1 we can't use upper case letters
+#2 we can't usenumbers
+#3 we can't use other languages
+price = 77
+discount = 20
+new_price = price - discount
+print(new_price)
---
+++ level 8/homework.py/hw2.py
@@ -0,0 +1,3 @@
+user_firstname = input("Please enter your firstname:")
+user_lastname = input("Please enter your lastname:")
+user_age = input("please enter youer age:")
