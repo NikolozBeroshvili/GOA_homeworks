3957bfffe51dfcc94d02e73b607a118c3db260fd
NikolozBeroshvili <nikolozbero0@gmail.com>
2024-07-31 19:55:35 +0400

    second commit

---
+++ level 12/homework/hw.py
@@ -0,0 +1,13 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter 2nd number: ")
+num3 = input("Enter last number: ")
+print(num1 + num2 + num3)
+
+
+
+name = input("Please enter your name: ")
+lastname = input("Please enter your Lastname: ")
+age = input("Please enter your Age: ")
+print("My name is" + " " + name )
+print("My lastname is" + " " + lastname  )
+print("I am" + " " + age + " " + "Years old")
