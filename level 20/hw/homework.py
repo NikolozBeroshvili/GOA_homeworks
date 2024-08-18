8bbd7f271acb4ff707367945b0f1da519a8de924
NikolozBeroshvili <nikolozbero0@gmail.com>
2024-08-18 10:32:12 +0400

    second commit

---
+++ level 20/hw/homework.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+
+if num == 18:
+    print("Congratulations! You won the toy dinosaur!")
+elif num == 33:
+    print("Congratulations! You won the toy rabbit!")
+else:
+    print("Sorry, you did not win anything. Try again in the future.")
